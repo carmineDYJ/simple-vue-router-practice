f8e64f709210114651fee572fc313dc6c52534e1
carmineding <carmineding@tencent.com>
2021-09-16 18:53:22 +0800

    simplify codes

--- src/main.js
+++ src/main.js
@@ -6,34 +6,24 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-
-let hash = window.location.hash;
-let hashNum = hash.substr(1);
-hashNum = hashNum || 1;
-console.log(`#d${hashNum}`);
-let div = window.document.querySelector(`#d${hashNum}`);
-if (div) {
-  div.style.display = "block";
-} else {
-  div = window.document.querySelector(`#d404`);
-  div.style.display = "block";
-}
-console.log(div);
-let history = div;
-
-window.addEventListener("hashchange", function () {
-  hash = window.location.hash;
-  hashNum = hash.substr(1);
-  console.log("#"+hashNum);
-  div = window.document.querySelector(`#d${hashNum}`);
-  console.log(div);
-  if (div) {
-    div.style.display = "block";
-  } else {
+let history;
+function simpleRouter() {
+  let hash = window.location.hash;
+  let hashNum = hash.substr(1);
+  hashNum = hashNum || 1;
+  let div = window.document.querySelector(`#d${hashNum}`);
+  console.log(!div);
+  if (!div) {
     div = window.document.querySelector(`#d404`);
-    div.style.display = "block";
   }
-  history.style.display = "none";
+  div.style.display = "block";
+  if(history)
+    history.style.display = "none";
+  console.log(div);
   history = div;
-})
+}
+simpleRouter();
+
+window.addEventListener("hashchange", ()=>{
+  simpleRouter();
+});
