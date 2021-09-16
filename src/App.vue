e2fc7ec07d08d71a304911d9bc468eab5e534a71
carmineding <carmineding@tencent.com>
2021-09-16 18:33:45 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a href="#1">go to 1</a>
+    <a href="#2">go to 2</a>
+    <a href="#3">go to 3</a>
+    <a href="#4">go to 4</a>
+    <div id="d1" style="display: none">1</div>
+    <div id="d2" style="display: none">2</div>
+    <div id="d3" style="display: none">3</div>
+    <div id="d4" style="display: none">4</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      user:{},
+      message: "Hi",
+      checkedNames: [],
+    };
+  },
+  components: {},
+  methods:{
+    onSubmit(){
+      alert(this.user);
+      console.log(this.user);
+    }
   }
-}
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -6,3 +6,24 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+let hash = window.location.hash;
+let hashNum = hash.substr(1);
+hashNum = hashNum || 1;
+console.log(`#d${hashNum}`);
+let div = window.document.querySelector(`#d${hashNum}`);
+div.style.display = "block";
+console.log(div);
+let history = window.document.querySelector(`#d${hashNum}`);
+
+window.addEventListener("hashchange", function () {
+  hash = window.location.hash;
+  hashNum = hash.substr(1);
+  console.log("#"+hashNum);
+  div = window.document.querySelector(`#d${hashNum}`);
+  console.log(div);
+  div.style.display = "block";
+  history.style.display = "none";
+  history = window.document.querySelector(`#d${hashNum}`);
+})
